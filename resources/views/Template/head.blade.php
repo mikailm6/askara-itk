0e4ddb08b9fd3c1facea46ad50d04a2211210442
HitmeMan <105223750+HitmeMan@users.noreply.github.com>
2023-04-02 15:19:58 +0800

    add dashboard admin

---
+++ resources/views/Template/head.blade.php
@@ -0,0 +1,15 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Askara-ITK</title>
+
+<!-- add icon link -->
+<link rel = "icon" href = 
+"{{asset('LogoAskara.png')}}" 
+        type = "image/x-icon">
+<!-- Google Font: Source Sans Pro -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+<!-- Font Awesome Icons -->
+<link rel="stylesheet" href="{{ asset('AdminLTE/plugins/fontawesome-free/css/all.min.css') }}">
+<!-- Theme style -->
+<link rel="stylesheet" href="{{ asset('AdminLTE/dist/css/adminlte.min.css') }}">
+@vite(['resources/sass/app.scss', 'resources/js/app.js'])
